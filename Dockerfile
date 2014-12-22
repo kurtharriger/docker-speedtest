9d928501d72f5d48cbf7365cbe7683e651b23e22
Kurt Harriger <kurtharriger@gmail.com>
2014-12-22 12:01:35 -0700

    Fix process hangs and stops testing
    Set socket timeout in speedtest-cli so that python process does not hang indefinately
    Also it appears nc can also hang sending udp, instead pipe to dev/udp

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM ubuntu
-RUN sudo apt-get update
-RUN sudo apt-get -y install python-pip
-RUN pip install speedtest-cli
-ADD test.sh /root/test.sh
+RUN apt-get update
+RUN apt-get -y install python
+ADD https://raw.githubusercontent.com/kurtharriger/speedtest-cli/master/speedtest_cli.py /usr/local/bin/speedtest-cli
+RUN chmod +x /usr/local/bin/speedtest-cli
+ADD test.sh /usr/local/bin/test.sh
 ENV STATSD_HOST 192.168.59.103
 ENV STATSD_PORT 8125
 ENV FREQUENCY 300
-CMD ["/root/test.sh"]
+CMD ["test.sh"]
--- test.sh
+++ test.sh
@@ -5,17 +5,28 @@ while true; do
 
   echo "$(date "$DF") Testing..."
 
-  R=$(speedtest-cli --simple)
-  PING=$(echo $R | sed -n -e 's/.*Ping: \([0-9\.]*\).*/\1/p')
-  DOWN=$(echo $R | sed -n -e 's/.*Download: \([0-9\.]*\).*/\1/p')
-  UP=$(echo $R | sed -n -e 's/.*Upload: \([0-9\.]*\).*/\1/p')
+  R=$(speedtest-cli --simple 2>>/var/log/speedtest.err | tee -a /var/log/speedtest.out)
+  if [ $? -eq 0 ]; then
+    PING=$(echo $R | sed -n -e 's/.*Ping: \([0-9\.]*\).*/\1/p')
+    DOWN=$(echo $R | sed -n -e 's/.*Download: \([0-9\.]*\).*/\1/p')
+    UP=$(echo $R | sed -n -e 's/.*Upload: \([0-9\.]*\).*/\1/p')
 
-  echo -n "$(date "$DF") "
-  echo $R | tr '\n' ' '
+    echo -n "$(date "$DF") "
+    echo -n $R | tr '\n' ' '
+    echo ""
 
-  echo "speedtest.ping:$PING|g" | nc -u -w0 $STATSD_HOST $STATSD_PORT
-  echo "speedtest.down:$DOWN|g" | nc -u -w0 $STATSD_HOST $STATSD_PORT
-  echo "speedtest.up:$UP|g" | nc -u -w0 $STATSD_HOST $STATSD_PORT
+    echo "speedtest.ping:$PING|g" > /dev/udp/$STATSD_HOST/$STATSD_PORT
+    echo "speedtest.down:$DOWN|g" > /dev/udp/$STATSD_HOST/$STATSD_PORT
+    echo "speedtest.up:$UP|g" > /dev/udp/$STATSD_HOST/$STATSD_PORT
+  else 
+    echo "$(date "$DF") Timeout occured"
 
+    echo "speedtest.timeout:1|c" > /dev/udp/$STATSD_HOST/$STATSD_PORT
+    echo "speedtest.down:0|g" > /dev/udp/$STATSD_HOST/$STATSD_PORT
+    echo "speedtest.up:0|g" > /dev/udp/$STATSD_HOST/$STATSD_PORT
+  fi
+
+  echo "speedtest.completed:1|c" > /dev/udp/$STATSD_HOST/$STATSD_PORT
+  echo "$(date "$DF") Pausing for $FREQUENCY seconds."
   sleep $FREQUENCY
 done
